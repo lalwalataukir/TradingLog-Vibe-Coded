16ffdcf1798ba2e750f11805d9490a774ee9b085
lalwalataukir <lalwalataukir>
2026-03-15 15:31:43 +0530

    Remove trash code

--- hooks/useTheme.ts
+++
@@ -1,8 +0,0 @@
-import { Colors, ThemeColors } from '@/constants/theme';
-import { useColorScheme } from 'react-native';
-
-export function useTheme(): { colors: ThemeColors; isDark: boolean } {
-    const scheme = useColorScheme();
-    const isDark = scheme !== 'light';
-    return { colors: isDark ? Colors.dark : Colors.light, isDark };
-}
